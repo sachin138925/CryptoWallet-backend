541be2ff0b44edcf86ea8c9bead253d819981ac6
sachin <sachinmahato1389.com>
2025-07-28 21:21:44 +0300

    test

--- server.js
+++ server.js
@@ -5,16 +5,13 @@ const cors = require("cors");
 
 const app = express();
 
-// Use a simple, wide-open CORS policy for this test
 app.use(cors());
 
-// A simple test route that does not require any other files
-app.get("/api/health", (req, res) => {
-    // This message proves the server is running.
-    res.status(200).json({ status: "The bare minimum server is alive!" });
-});
+// We are NOT adding the JSON parser yet, to keep it simple
+// app.use(express.json());
 
-// We are not including the main router to avoid dependency errors
-// app.use("/api", require("./routes/apiRoutes.js"));
+// --- ADDING THE ROUTER BACK ---
+// If the server crashes now, the error is in one of the files required by apiRoutes.js
+app.use("/api", require("./routes/apiRoutes.js"));
 
 module.exports = app;
